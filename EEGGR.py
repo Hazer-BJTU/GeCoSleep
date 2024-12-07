b67552bac7bf2e36413bfa96f4a3d35e7c3fa3ac
Hazer <3169443421@qq.com>
2024-12-07 12:09:17 +0800

    2024/12/7-lcz-train-generator-solver

--- EEGGR.py
+++ EEGGR.py
@@ -8,6 +8,8 @@ from generator import *
 class EEGGRnetwork(CLnetwork):
     def __init__(self, args, fold_num):
         super(EEGGRnetwork, self).__init__(args, fold_num)
+        '''generator settings'''
+        self.start_training_generator = False
         self.num_epochs_solver = self.args.num_epochs - self.args.num_epochs_generator
         self.generator = EEGVAE(2)
         self.generator.apply(init_weight)
@@ -19,12 +21,15 @@ class EEGGRnetwork(CLnetwork):
 
     def start_task(self):
         super(EEGGRnetwork, self).start_task()
+        '''generator settings'''
+        self.start_training_generator = False
         self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, max(self.num_epochs_solver // 6, 1), 0.6)
         self.optimizerG = torch.optim.Adam(self.generator.parameters(), lr=self.args.lr_generator)
         self.schedulerG = torch.optim.lr_scheduler.StepLR(self.optimizerG, max(self.args.num_epochs_generator // 6, 1), 0.6)
 
     def start_epoch(self):
         super(EEGGRnetwork, self).start_epoch()
+        '''generator settings'''
         self.rec_loss, self.kl_loss, self.feautre_loss = 0, 0, 0
         self.generator.train()
 
@@ -32,10 +37,15 @@ class EEGGRnetwork(CLnetwork):
         if self.epoch < self.num_epochs_solver:
             super(EEGGRnetwork, self).observe(X, y, first_time)
         else:
-            if self.epoch == self.num_epochs_solver:
+            if not self.start_training_generator:
                 print('start training generator...')
                 self.net.load_state_dict(torch.load(self.best_net, map_location=self.device, weights_only=True))
-                print('best solver model loaded...')
+                print(f'best solver model loaded: {self.best_net}')
+                self.start_training_generator = True
+            '''freeze BN and dropout'''
+            for m in self.net.modules():
+                if isinstance(m, nn.BatchNorm1d) or isinstance(m, nn.Dropout):
+                    m.eval()
             X, y = X.to(self.device), y.to(self.device)
             self.optimizerG.zero_grad()
             self.optimizer.zero_grad()
@@ -60,9 +70,9 @@ class EEGGRnetwork(CLnetwork):
                   f"1000 lr: {self.optimizerG.state_dict()['param_groups'][0]['lr'] * 1000:.3f}")
             self.epoch += 1
             self.schedulerG.step()
-        if self.epoch + 1 == self.args.num_epochs:
-            generator_path = './modelsaved/generator_task' + str(self.task) + '_fold' + str(self.flod_num) + '.pth'
-            torch.save(self.generator.state_dict(), generator_path)
+            if self.epoch + 1 == self.args.num_epochs:
+                generator_path = './modelsaved/generator_task' + str(self.task) + '_fold' + str(self.flod_num) + '.pth'
+                torch.save(self.generator.state_dict(), generator_path)
 
     def end_task(self):
         super(EEGGRnetwork, self).end_task()
