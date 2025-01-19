36a1f66b30ba43e704b7483d9ad7409b33edb3f7
Hazer <3169443421@qq.com>
2025-01-19 20:12:47 +0800

    2025/1/19-lcz-hierarchical-generator

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ parser.add_argument('--num_epochs_generator', type=int, nargs='?', default=100, 
 parser.add_argument('--lr_seq_gen', type=float, nargs='?', default=1e-4, help='learning rate for sequential generator')
 parser.add_argument('--alpha', type=float, nargs='?', default=1, help='coefficient of task loss')
 parser.add_argument('--beta', type=float, nargs='?', default=0.1, help='coefficient of kl loss')
+parser.add_argument('--gamma', type=float, nargs='?', default=0.1, help='updating rate for running loss')
 args = parser.parse_args()
 
 if __name__ == '__main__':
