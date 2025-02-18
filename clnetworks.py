15070d6f5f04399280ce3f799c41573c54f365a4
Hazer <3169443421@qq.com>
2025-02-18 21:55:50 +0800

    2025/2/18-lcz-logs

--- clnetworks.py
+++ clnetworks.py
@@ -3,9 +3,10 @@ from metric import *
 
 
 class CLnetwork:
-    def __init__(self, args, fold_num):
+    def __init__(self, args, fold_num, logs):
         self.args = args
-        self.flod_num = fold_num
+        self.fold_num = fold_num
+        self.logs = logs
         self.num_channels = len(args.isruc1)
         self.net = SleepNet(self.num_channels, args.dropout)
         self.net.apply(init_weight)
@@ -53,6 +54,12 @@ class CLnetwork:
         train_acc, train_mf1 = self.confusion_matrix.accuracy(), self.confusion_matrix.macro_f1()
         print(f'epoch: {self.epoch}, train loss: {self.train_loss / self.cnt:.3f}, train accuracy: {train_acc:.3f}, '
               f"macro F1: {train_mf1:.3f}, 1000 lr: {self.optimizer.state_dict()['param_groups'][0]['lr'] * 1000:.3f}")
+        self.logs.append(['train_info', f'task{self.task}_fold{self.fold_num}', f'epoch:{self.epoch}'], {
+            'train loss': self.train_loss / self.cnt,
+            'train accuracy': train_acc,
+            'train mF1': train_mf1,
+            '1000 lr': self.optimizer.state_dict()['param_groups'][0]['lr'] * 1000
+        })
         if (self.epoch + 1) % self.args.valid_epoch == 0:
             print(f'validating on the datasets...')
             valid_confusion = ConfusionMatrix(1)
@@ -60,7 +67,12 @@ class CLnetwork:
                                              self.device, self.args.valid_batch)
             valid_acc, valid_mf1 = valid_confusion.accuracy(), valid_confusion.macro_f1()
             print(f'valid accuracy: {valid_acc:.3f}, valid macro F1: {valid_mf1:.3f}')
+            self.logs.append(['train_info', f'task{self.task}_fold{self.fold_num}', f'valid epoch:{self.epoch}'], {
+                'valid accuracy': valid_acc,
+                'valid mF1': valid_mf1
+            })
             if valid_acc + valid_mf1 > self.best_valid_acc and self.epoch + 1 >= self.args.min_epoch:
+                self.logs.append(['train_info', f'task{self.task}_fold{self.fold_num}', f'valid epoch:{self.epoch}', 'saved'], True)
                 self.best_train_loss = self.train_loss / self.cnt
                 self.best_train_acc = train_acc
                 self.best_valid_acc = valid_acc + valid_mf1
@@ -102,6 +114,68 @@ class Independent(CLnetwork):
         self.best_train_loss, self.best_train_acc, self.best_valid_acc = 0.0, 0.0, 0.0
         self.optimizer = torch.optim.AdamW(self.net.parameters(), lr=self.args.lr, weight_decay=self.args.weight_decay)
         self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, max(self.args.num_epochs // 6, 1), 0.6)
+
+
+class ExperienceReplay(CLnetwork):
+    def __init__(self, args, fold_num, logs):
+        super(ExperienceReplay, self).__init__(args, fold_num, logs)
+        self.replay_buffer = self.args.replay_buffer
+        self.sample_buffer = None
+        self.label_buffer = None
+        self.replay_memory = []
+
+    def start_task(self):
+        super(ExperienceReplay, self).start_task()
+        self.sample_buffer = None
+        self.label_buffer = None
+
+    def update_buffer(self, X, y):
+        if self.sample_buffer is None or self.label_buffer is None:
+            if X.shape[0] >= self.replay_buffer:
+                self.sample_buffer = X[:self.replay_buffer, :, :, :]
+                self.label_buffer = y[:self.replay_buffer, :]
+            else:
+                self.sample_buffer = X
+                self.label_buffer = y
+        else:
+            remaining = self.replay_buffer - self.sample_buffer.shape[0]
+            if remaining == 0:
+                return
+            if X.shape[0] >= remaining:
+                self.sample_buffer = torch.cat((self.sample_buffer, X[:remaining, :, :, :]), dim=0)
+                self.label_buffer = torch.cat((self.label_buffer, y[:remaining, :]), dim=0)
+            else:
+                self.sample_buffer = torch.cat((self.sample_buffer, X), dim=0)
+                self.label_buffer = torch.cat((self.label_buffer, y), dim=0)
+
+    def observe(self, X, y, first_time=False):
+        if first_time:
+            self.update_buffer(X, y)
+        X, y = X.to(self.device), y.to(self.device)
+        self.optimizer.zero_grad()
+        y_hat = self.net(X)
+        L_current = self.loss(y_hat, y.view(-1))
+        L = torch.mean(L_current)
+        if self.task > 0:
+            self.net.freeze_parameters()
+            total_number = 0
+            for Xr, yr in self.replay_memory:
+                total_number += Xr.shape[0]
+            print(f'perform experience replay on {len(self.replay_memory)} tasks and {total_number} samples...')
+            for Xr, yr in self.replay_memory:
+                Xr, yr = Xr.to(self.device), yr.to(self.device)
+                yr_hat = self.net(Xr)
+                L_replay = self.loss(yr_hat, yr.view(-1))
+                L = L + torch.mean(L_replay) / len(self.replay_memory)
+        L.backward()
+        self.optimizer.step()
+        self.train_loss += L.item()
+        self.cnt += 1
+        self.confusion_matrix.count_task_separated(y_hat, y, 0)
+
+    def end_task(self):
+        super(ExperienceReplay, self).end_task()
+        self.replay_memory.append((self.sample_buffer, self.label_buffer))
 
 
 if __name__ == '__main__':
